3b705cb80e47cb8d0d185fd583a68819e7cbf4c2
Oliver Batchelor <saulzar@gmail.com>
2024-01-20 22:21:43 +1300

    Run multiple benchmarks on different scenes at same time

--- splat_viewer/scripts/splat_benchmark.py
+++ splat_viewer/scripts/splat_benchmark.py
@@ -18,6 +18,7 @@ from torch.profiler import profile, record_function, ProfilerActivity
 
 
 def bench_forward(gaussians, renderer, cameras, profiler=None, **kwargs):
+
     inputs = renderer.pack_inputs(gaussians)
     torch.cuda.synchronize()
 
@@ -49,42 +50,11 @@ def bench_backward(gaussians, renderer, cameras, profiler=None, **kwargs):
     torch.cuda.synchronize()
     return time.time() - start_time
 
-def main():
-
-  torch.set_printoptions(precision=5, sci_mode=False, linewidth=120)
-
-  parser = argparse.ArgumentParser()
-
-  parser.add_argument("model_path", type=Path, help="workspace folder containing cameras.json, input.ply and point_cloud folder with .ply models")
-  parser.add_argument("--device", type=str, default="cuda:0", help="torch device to use")
-  parser.add_argument("--model", type=str, default=None, help="model iteration to load from point_clouds folder")
-
-  parser.add_argument("--profile", action="store_true", help="enable profiling")
-  parser.add_argument("--trace", type=str, default=None, help="enable profiling with tensorboard trace for profiled data")
-  parser.add_argument("--backward", action="store_true", help="benchmark backward pass")
-
-  add_render_arguments(parser)
-
-  parser.add_argument("--debug", action="store_true", help="enable taichi kernels in debug mode")
-  parser.add_argument("-n", type=int, default=500, help="number of iterations to render")
-
-  parser.add_argument("--sh_degree", type=int, default=None, help="modify spherical harmonics degree")
-  parser.add_argument("--no_sort", action="store_true", help="disable sorting by scale (sorting makes tilemapping faster)")
 
 
 
-  parser.add_argument("--depth", action="store_true", help="render depth maps")
-  parser.add_argument("--image_size", type=int, default=None, help="resize longest edge of camera image sizes")
-
-
-  
-  args = parser.parse_args()
-
-  ti.init(arch=ti.cuda, offline_cache=True, log_level=ti.INFO,
-          debug=args.debug, device_memory_GB=0.1)
-
-
-  workspace = load_workspace(args.model_path)
+def benchmark_model(model_path:Path, args):
+  workspace = load_workspace(model_path)
   if args.model is None:
       args.model = workspace.latest_iteration()
     
@@ -105,7 +75,7 @@ def main():
   def n_cameras(n):
     return list(itertools.islice(itertools.cycle(cameras), n))
 
-  print(f"Benchmarking {args.model_path} with {gaussians.batch_size[0]} points")
+  print(f"Benchmarking {model_path} with {gaussians.batch_size[0]} points")
 
   image_sizes = set([tuple(camera.image_size) for camera in cameras])
   print(f"Cameras: {len(workspace.cameras)}, Image sizes: {image_sizes}")
@@ -113,21 +83,22 @@ def main():
 
   bench_renders = bench_backward if args.backward else bench_forward
 
-  warmup_size = args.n // 10
+
+  warmup_size = 10
   print(f"Warmup @ {warmup_size} cameras")
   bench_renders(gaussians, renderer, n_cameras(warmup_size), render_depth=args.depth)
 
-
-  print(f"Benchmark @ {args.n} cameras:")
-
+  render_cameras = cameras if args.n is None else n_cameras(args.n)
+  num_cameras = len(render_cameras)
+  print(f"Benchmark @ {len(render_cameras)} cameras:")
 
   if args.profile or args.trace:
     with profile(activities=[ProfilerActivity.CUDA], 
                 record_shapes=True, with_stack=True) as prof:
       with record_function("model_inference"):
-        ellapsed = bench_renders(gaussians, renderer, n_cameras(args.n), profiler=prof, render_depth=args.depth)
+        ellapsed = bench_renders(gaussians, renderer, render_cameras, profiler=prof, render_depth=args.depth)
     result = prof.key_averages(group_by_stack_n=4).table(sort_by="self_cuda_time_total", 
-                                       row_limit=25, max_name_column_width=100)
+                                      row_limit=25, max_name_column_width=100)
     
     if args.trace:
       print(f"Writing chrome trace file to {args.trace}")
@@ -137,10 +108,56 @@ def main():
       print(result)
 
   else:
-    ellapsed = bench_renders(gaussians, renderer, n_cameras(args.n), render_depth=args.depth)
-  print(f"{args.n} images in {ellapsed:.2f}s at {args.n / ellapsed:.2f} images/s")
+    ellapsed = bench_renders(gaussians, renderer, render_cameras, render_depth=args.depth)
+  print(f"{num_cameras} images in {ellapsed:.2f}s at {num_cameras / ellapsed:.2f} images/s")
 
-    
+  return dict(ellapsed=ellapsed, num_cameras=num_cameras, rate=num_cameras / ellapsed)
+
+
+def main():
+
+  torch.set_printoptions(precision=5, sci_mode=False, linewidth=120)
+
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument("model_paths", type=Path, nargs='+', help="workspace folder containing cameras.json, input.ply and point_cloud folder with .ply models")
+
+  parser.add_argument("--device", type=str, default="cuda:0", help="torch device to use")
+  parser.add_argument("--model", type=str, default=None, help="model iteration to load from point_clouds folder")
+
+  parser.add_argument("--profile", action="store_true", help="enable profiling")
+  parser.add_argument("--trace", type=str, default=None, help="enable profiling with tensorboard trace for profiled data")
+  parser.add_argument("--backward", action="store_true", help="benchmark backward pass")
+
+  add_render_arguments(parser)
+
+  parser.add_argument("--debug", action="store_true", help="enable taichi kernels in debug mode")
+  parser.add_argument("-n", type=int, default=None, help="number of iterations to render (default all cameras))")
+
+  parser.add_argument("--sh_degree", type=int, default=None, help="modify spherical harmonics degree")
+  parser.add_argument("--no_sort", action="store_true", help="disable sorting by scale (sorting makes tilemapping faster)")
+
+
+
+  parser.add_argument("--depth", action="store_true", help="render depth maps")
+  parser.add_argument("--image_size", type=int, default=None, help="resize longest edge of camera image sizes")
+
+
+  
+  args = parser.parse_args()
+
+  ti.init(arch=ti.cuda, offline_cache=True, log_level=ti.INFO,
+          debug=args.debug, device_memory_GB=0.1)
+
+  summary = {}
+
+  for model_path in args.model_paths:
+    summary[model_path] = benchmark_model(model_path, args)
+
+  
+  print("Summary:")
+  for model_path, info in summary.items():
+    print(f"{model_path}({info['num_cameras']}): {info['rate']:.2f} images/s")
 
 if __name__ == "__main__":
   main()  
