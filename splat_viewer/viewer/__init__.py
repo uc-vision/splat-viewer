5963ad7c90b87f7c3c6f849308d484a1d016e12f
Oliver Batchelor <saulzar@gmail.com>
2024-06-02 21:38:55 +1200

    Add init.py

---
+++ splat_viewer/viewer/__init__.py
@@ -0,0 +1,7 @@
+from .viewer import (init_viewer, show_workspace, 
+                     ViewerProcess, Viewer)
+
+from .settings import (Settings, Show)
+
+__all__ = ["init_viewer", "show_workspace",
+           "ViewerProcess", "Settings", "Show", "Viewer"]
