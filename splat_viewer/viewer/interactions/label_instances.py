41d485bec72ccc58aa5a9fa797e438666665599c
Oliver Batchelor <oliver.batchelor@canterbury.ac.nz>
2024-09-17 01:31:03 +1200

    WIP

--- splat_viewer/viewer/interactions/label_instances.py
+++ splat_viewer/viewer/interactions/label_instances.py
@@ -11,41 +11,14 @@ from splat_viewer.viewer.interaction import Interaction
 
 
 
-def in_sphere(positions:torch.Tensor, center:torch.Tensor, radius:float):
-  idx = in_box(positions, center - radius, center + radius)
-  return idx[torch.linalg.norm(positions[idx] - center, dim=-1) <= radius]
-
-def in_box(positions:torch.Tensor, lower:torch.Tensor, upper:np.array):
-  mask = ((positions >= lower) & (positions <= upper)).all(dim=-1)
-  return torch.nonzero(mask, as_tuple=True)[0]
-
-
-class ScribbleGeometric(Interaction):
+class LabelInstances(Interaction):
   def __init__(self):
-    super(ScribbleGeometric, self).__init__()
+    super(LabelInstances, self).__init__()
 
-    self.drawing = False
+    self.instances = []
 
-    self.current_label = 0
-    self.current_points = None
-
-    self.color = torch.tensor([1, 0, 0], dtype=torch.float32)
-
-  @property
-  def ready(self):
-    return bool(self.modifiers & Qt.ControlModifier)
-  
-
-  def mousePressEvent(self, event: QtGui.QMouseEvent):
-    if event.button() == Qt.LeftButton and event.modifiers() & Qt.ControlModifier:
-      self.drawing = True
-      self.draw((event.x(), event.y()))
-      return True
     
-  def mouseReleaseEvent(self, event: QtGui.QMouseEvent):
-    if event.button() == Qt.LeftButton and self.drawing:
-      self.drawing = False
-      return True
+
     
   def draw(self, cursor_pos:Tuple[int, int]):
     depth = self.lookup_depth(cursor_pos)
