30deaac7e00ab16f607f075077d8bef9d0c54ef5
Oliver Batchelor <oliver.batchelor@canterbury.ac.nz>
2025-10-02 00:07:53 +1300

    .

--- splat_viewer/viewer/__init__.py
+++
@@ -1,7 +0,0 @@
-from .viewer import (init_viewer, show_workspace, 
-                     ViewerProcess, Viewer)
-
-from .settings import (Settings, Show)
-
-__all__ = ["init_viewer", "show_workspace",
-           "ViewerProcess", "Settings", "Show", "Viewer"]
